af20d16e8e36c72bdf9be101ca55067512d38e04
Chris Dinh <dinh.thanhtoan@gmail.com>
2021-02-10 23:12:11 +1300

    Add GetSMoSMinimumHexStringLength()

--- projects/smos-js/src/lib/smos-js.service.ts
+++ projects/smos-js/src/lib/smos-js.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
 
+export enum SMoSDefinitions {
+  /* SMoS minimum Hex string length (i.e. when byte count is 0) */
+  SMOS_HEX_STRING_MIN_LENGTH = 11,
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class SmosJsService {
 
   constructor() { }
+
+  /* Returns the value of SMOS_HEX_STRING_MIN_LENGTH, this can be useful
+     when receiving data across the serial link one char at a time. */
+  public GetSMoSMinimumHexStringLength(): number {
+    return SMoSDefinitions.SMOS_HEX_STRING_MIN_LENGTH;
+  }
 }
